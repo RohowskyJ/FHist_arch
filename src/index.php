f79a75ca043526efb9ca018a56a3f2ffdf0f97e5
Josef Rohowsky <josef@kexi.at>
2025-02-05 17:47:35 +0100

    initial

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "<!DOCTYPE html>";
+echo "<html lang='de'>"; # style='overflow-x:scroll;'
+echo "<head>";
+echo "  <meta charset='UTF-8'>";
+echo "  <title>Feuerwehrhistoriker in NÖ</title>";
+echo "  <meta  name='viewport' content='width=device-width, initial-scale=1.00'>";
+echo '<meta name="description" content="Feuerwehrhistoriker Dokumentationen - Archiv, Inventar, Beschreibungen, Kataloge, ...">';
+echo "<meta name='copyright' content='FT Ing. Josef Rohowsky 2020-2025'>";
+echo '<meta name="robots" content="noindex">';
+echo '<meta name="robots" content="nofollow">';
+
+echo "<link rel='icon' type='image/x-icon' href='login/common/imgs/favicon.ico'>";
+
+echo "</head>";
+echo "<body>";
+
+echo "<h1>Diese HomePage wird auf WordPress umgestellt</h1>";
+
+echo "<b><p>Die Nutzung der original- Seite ist nach wie vor möglich, aber nur zur Ansicht.
+ Derzeit keine Eigaben möglich (Datensicherheit - für den Fall das bei der Instllation von WP etwas schiefgeht.</b></p>";
+
+echo "<b><a href='VFH/index.php' target='orig'>Zur Original- Seite</a></b>";
+
+			
+echo "</body>";
+?>
