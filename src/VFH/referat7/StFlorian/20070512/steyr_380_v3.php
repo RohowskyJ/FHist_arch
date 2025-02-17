fe62dd2589407115abc8f8249dc0732641fa6ab4
Josef Rohowsky <josef@kexi.at>
2025-02-17 15:17:38 +0100

    anpassung

--- src/VFH/referat7/StFlorian/20070512/steyr_380_v3.php
+++ src/VFH/referat7/StFlorian/20070512/steyr_380_v3.php
@@ -10,7 +10,7 @@ $path2ROOT = "../../../../";
 
 $debug = False;  // Debug output Ein/Aus Schalter
 
- require $path2ROOT.'login/common/Funcs.inc.php' ;  // Diverse Unterprogramme
+ require $path2ROOT.'login/common/BA_Funcs.lib.php' ;  // Diverse Unterprogramme
 
 initial_debug(); # Wenn $debug=true - Ausgabe von Debug Informationen: $_POST, $_GET, $_FILES
 
