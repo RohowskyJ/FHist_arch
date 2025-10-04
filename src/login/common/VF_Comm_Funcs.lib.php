a6a4ab612a578be8fb78cb2f306ee374fcefe98f
Josef Rohowsky <josef@kexi.at>
2025-10-04 18:08:57 +0200

    Erweitrung der Archivordnungs Ebenen um zwei Felder

--- src/login/common/VF_Comm_Funcs.lib.php
+++ src/login/common/VF_Comm_Funcs.lib.php
@@ -399,7 +399,7 @@ function VF_Displ_Aera($fz_area, $sub_funct)
  * @global array $neu Eingelesene Daten Felder
  * @global string $module Modul-Name für $_SESSION[$module] - Parameter
  */
-function VF_Displ_Arl($sg, $ssg, $lcsg, $lcssg)
+function VF_Displ_Arl($sg, $ssg, $lcsg, $lcssg,$lcssg_s0, $lcssg_s1)
 {
     global $debug, $db, $neu, $module, $flow_list;
 
@@ -416,15 +416,16 @@ function VF_Displ_Arl($sg, $ssg, $lcsg, $lcssg)
     $table = "ar_ord_local"; # . $_SESSION['Eigner']['eig_eigner'];
     // `al_eigner`='".$_SESSION[$module]['eignr']."' AND
     $lcssg_val = "";
-    if ($lcssg != "" && $lcssg != "0") {
-        $lcssg_val =  " AND `al_lcssg` = '$lcssg' ";
-    }
+    #if ($lcssg != "" && $lcssg != "0") {
+        $lcssg_val =  " AND `al_lcssg` = '$lcssg' AND al_lcssg_s0 = '$lcssg_s0' AND al_lcssg_s1 = '$lcssg_s1' ";
+    #}
     $sql_al = "SELECT * FROM $table WHERE   `al_sg`='$sg.$ssg' AND `al_lcsg` = '$lcsg' $lcssg_val ORDER BY al_sg ASC";
     $return_al = mysqli_query($db, $sql_al); // or die("Datenbankabfrage gescheitert. ".mysqli_error($db));
-    # print_r($return_al);echo "<br> L 211: return \$sql_al $sql_al <br>";
+    # print_r($return_al);echo "<br> L 424: return \$sql_al $sql_al <br>";
     if ($return_al) {
         while ($row = mysqli_fetch_object($return_al)) {
-            $string_arl = "$row->al_lcsg.$row->al_lcssg $row->al_bezeich";
+            # var_dump($row);
+            $string_arl = "$row->al_lcsg|$row->al_lcssg|$row->al_lcssg_s0|$row->al_lcssg_s1|$row->al_sammlung|$row->al_bezeich";
         }
         mysqli_free_result($return_al);
     }
@@ -2035,7 +2036,7 @@ function VF_Multi_Sel_Input()
 } # Ende Function VF_Multi_Sel_input
 
 /**
- * AuswahlListe der EigentÃ¼mer fÃ¼r select Liste
+ * AuswahlListe der Eigentümer für select Liste
  *
  * @param string $FeldName
  *            Name des Eingabefeldes
@@ -2825,185 +2826,7 @@ $(document).ready(function() {
 <?php 
 
 } // end VF_Upload_Form_M
-/*
-function VF_M_Foto_N_ori()
-{
-    global $debug, $db, $neu, $module, $Tabellen_Spalten_COMMENT, $flow_list, $hide_area, $path2ROOT;
 
-    flow_add($module, "VF_Upload.lib.php Funct: VF_M_Foto_N");
-
-    / **
-     * Parameter für die Fotos:
-     *
-     * $_SESSION[$module]['Pct_Arr'][] = array("k1" => 'fz_b_1_komm', 'b1' => 'fz_bild_1', 'rb1' => '', 'up_err1' => '', 'f1' => '','f2'=>'');
-     * wobei k1 = blank : kein Bild- Text- Feld - kein Bildtext , keinegemeinsame Box, rb1 und up_err werden vom Uploader gesetzt,
-     *                           f1 und f2 sind 2 Felder, die zusätzlich im Block eingegeben, angezeigt werden können
-     * /
-
-    if ($debug) {
-        echo "<pre class=debug>VF_M_Foto L Beg: \$Picts ";
-        var_dump($_SESSION[$module]['Pct_Arr']);
-        echo "<pre>";
-    }
-
-    $pic_cnt = count($_SESSION[$module]['Pct_Arr']);
-    console_log('L 048 Anz Upl. '.$pic_cnt);
-    #var_dump($_SESSION[$module]['Pct_Arr']);
-    # var_dump($neu);
-
-    / **
-     * Floating Block mit Bild, Bildbeschreibung , Bildname und Upload-Block
-     * /
-    echo "<div class='w3-container'>";                           // container für Foto und Beschreibung
-    #console_log('L 056 vor class w3-row ');
-    echo "<div class = 'w3-row w3-border'>";                     // Responsive Block start
-    echo "<fieldset>";
-    #console_log('L 059 vor pct_arr loop ');
-
-    # var_dump($_SESSION[$module]['Pct_Arr']);
-
-    ?>
-
-    <div style="margin-bottom:20px; border:1px solid #ccc; padding:10px;"> 
-       
-             <!-- Optional: Parameter-Input -->
-             <div class="file-upload-block" id="parameterBlock">
-                 <!-- Auswahl: Bibliothek oder Upload -->
-                 <p>Fotos aus den Foto-Bibliotheken einfügen?</p>
-                 <label>
-                     <input type='radio' class='sel_libs' name='sel_libs' value='Ja' checked> Ja
-                 </label>
-                 <label>
-                     <input type='radio' class='sel_libs' name='sel_libs' value='Nein'> Nein - Neu hochladen
-                 </label>
-        
-                
-                <!-- 
-                wenn sel_libs: Ja    -- Auswahl nach Sammlung, Anzeige fotos, auswahl in Menge der Fotos
-                                        ausgesuchter Dsn in das entsprechenden Eigabefeld stellen
-                               Nein  -- normale Eingabe über File-Upload, bevorzugt AJAX mit resize und Wasserzeichen,
-                                        Eingabe Urheber, Aufnahmedatum, Wasserzeichen J/N, 
-                --->
-                <div id='upl_libs' style='display:none'>
-                  <p>Aussuchen aus libs
-                   </p>
-                </div>
-              
-                <div id='upl_new' style='display:none'>
-                  hochladen
-                
-                </div>
-        
-                <div id='upl_libs' style='display:block; margin-top:10px;'>
-                 <p>Suchbegriff für die Bibliothek:</p>
-                 <input type='text' id='suche' placeholder='Suchbegriff' />
-                 <button type='button' onclick='sucheBibliothek()'>Suchen</button>
-                    <div id=' suchergebnis' style='margin-top:10px; max-height:150px; overflow:auto; border:1px solid #ccc;'></div>
-                </div>    
-            </div>
-
-    <?php
-
-    for ($i = 0; $i < $pic_cnt; $i++) {
-        $p_a = $_SESSION[$module]['Pct_Arr'][$i];
-
-        console_log('L 0107 foto '.$i);
-        $j = $i + 1;
-        #var_dump($p_a);echo "L 02504 hide_area $hide_area <br>";
-
-        #echo $neu[$p_a['ko']]. " ".$p_a['bi'] . " " . $neu[$p_a['f1']]." ". $p_a['f2'] ."<br>";
-
-        # if ($hide_area == 0 || ($hide_area == 1 && ($neu[$p_a['ko']] != '' || $neu[$p_a['bi']] != ''  ))) {
-        #console_log('L 02508 Bild '.$key);
-        # echo "Bild- Box $key wird angezeigt <br>";
-        $pict_path = VF_Upload_Pfad_M('', '', '', '');
-
-        / **
-         * Responsive Container innerhalb des loops
-         * /
-        echo "<div class = 'w3-container w3-half'>";                                  // start half contailer
-        echo "<fieldset>";
-        echo "Bild $j <br>";
-
-        if ($hide_area == 0 || ($hide_area == 1 && ($neu[$p_a['ko']] != '' || $neu[$p_a['bi']] != ''))) {
-
-            #console_log('L 0128 komm '.$key);
-            if ($p_a['ko'] != "") {
-                if (isset($Tabellen_Spalten_COMMENT[$p_a['ko']])) {
-                    echo $Tabellen_Spalten_COMMENT[$p_a['ko']];
-                } else {
-                    echo $p_a['ko'];
-                }
-                echo "<textarea class='w3-input' rows='7' cols='25' name='".$p_a['ko']."' >" . $neu[$p_a['ko']] . "</textarea> ";
-            }
-            if ($p_a['f1'] != '') {
-                Edit_Daten_Feld_Button($p_a['f1'], 30);
-            }
-            if ($p_a['f2'] != '') {
-                Edit_Daten_Feld_Button($p_a['f2'], 30);
-            }
-            if ($neu[$p_a['bi']] != "") {
-                $fo = $neu[$p_a['bi']];
-                #console_log('L 02528 foto '.$fo);
-                $fo_arr = explode("-", $neu[$p_a['bi']]);
-                $cnt_fo = count($fo_arr);
-
-                if ($cnt_fo >= 3) {   // URH-Verz- Struktur de dsn
-                    $urh = $fo_arr[0]."/";
-                    $verz = $fo_arr[1]."/";
-                    if ($cnt_fo > 3) {
-                        if (isset($fo_arr[3])) {
-                            $s_verz = $fo_arr[3]."/";
-                        }
-                    }
-                    $p = $path2ROOT ."login/AOrd_Verz/$urh/09/06/".$verz.$neu[$p_a['bi']] ;
-
-                    if (!is_file($p)) {
-                        $p = $pict_path . $neu[$p_a['bi']];
-                    }
-                } else {
-                    $p = $pict_path . $neu[$p_a['bi']];
-                }
-                #console_log('L 02547 foto '.$p) ;
-
-                $f_arr = pathinfo($neu[$p_a['bi']]);
-                if ($f_arr['extension'] == "pdf") {
-                    echo "<a href='$p' target='Bild $j' > Dokument</a>";
-                } else {
-                    #console_log('L 0110 ausgabe '.$p);
-                    echo "<a href='$p' target='Bild $j' > <img src='$p' alter='$p' width='200px'></a>";
-                    echo $neu[$p_a['bi']];
-                }
-
-            }
-
-            ## Bilder neu auswählen /Ändern
-
-            ?>
-
-        <div class='file-upload-block' id='uploadBlock<?php echo $j; ?>' style="margin-bottom:20px; border:1px solid #ccc; padding:10px;"> 
-          <div id='upl_new' >
-            <h3>Bild <?php echo $j; ?></h3>
-            <input type='hidden' id='foto_<?php echo $j; ?>' name='foto_<?php echo $j; ?>' value=''>
-
-            <!-- Hochladen Bereich -->
-            <div id='upl_new_<?php echo $j; ?>' margin-top:10px;'>
-                <input type='file' name='datei_<?php echo $j; ?>' />
-            </div>
-          </div>  
-        </div>
-
-        <?php
-        }
-        echo "</fieldset>";
-        echo "</div>";
-    }
-
-    echo "</fieldset>";
-    echo "</div>";  // Responsive Block end
-    echo "</div>";        // end container
-} // end VF_Upload_Form_M
-*/
 /**
  * Hochladen von Dateien
  *
