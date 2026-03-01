49aab7f50a7f53e02ab97d851262548b13e4fe67
Josef Rohowsky <josef@kexi.at>
2026-03-01 07:39:50 +0100

    anpassung

--- src/login/common/FS_Config.lib.php
+++ src/login/common/FS_Config.lib.php
@@ -3,4 +3,4 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'database');
 define('DB_USER', 'userid'); // Ersetzen Sie dies
-define('DB_PASS', 'password'); // Ersetzen Sie dies
+define('DB_PASS', 'password'); // Ersetzen Sie dies   
