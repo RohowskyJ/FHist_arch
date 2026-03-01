c0717a93ae8aae141332709c7208004ebe6af25e
Josef Rohowsky <josef@kexi.at>
2026-03-01 07:36:47 +0100

    kill

---
+++ src/login/common/FS_Config.lib.php
@@ -0,0 +1,6 @@
+<?php
+// config.php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'database');
+define('DB_USER', 'userid'); // Ersetzen Sie dies
+define('DB_PASS', 'password'); // Ersetzen Sie dies
