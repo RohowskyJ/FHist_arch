2a6d71fbe1fcc9e6d08e6d2f43416c4d9caaced7
Josef Rohowsky <josef@kexi.at>
2026-03-01 07:45:35 +0100

    delete

--- src/login/common/FS_Config.lib.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'fharch_new');
-define('DB_USER', 'root'); // Ersetzen Sie dies
-define('DB_PASS', 'b1teller'); // Ersetzen Sie dies
-define('DB_NAME', 'database');
-define('DB_USER', 'userid'); // Ersetzen Sie dies
-define('DB_PASS', 'password'); // Ersetzen Sie dies   
